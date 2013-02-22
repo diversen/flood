b8b77f12f5597e3804d54459c5a63eb64db2f8cc
dennis <dennis@dennis-Rev-1-0.(none)>
2013-02-22 15:22:02 +0100

    fix flood. translate

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ $post_next = strtotime($row['updated']) + $interval;
 $time_to_next_post = $post_next - time();
 
 if ($time_to_next_post < 0) {
-    echo "You should be able to post";
+    html::headline(lang::translate('flood: You can post agian'));
+    echo lang::translate('flood: You should be able to post');
     return;
 }
 
@@ -20,5 +21,18 @@ $res = time::getSecsDivided($time_to_next_post);
 
 //print_r($row );
 
-echo "Max Amount of posts is $max_posts per $interval secs\n";
-echo "Your post counter will be reset in $res[minutes] minutes and $res[seconds] seconds";
+
+html::headline(lang::translate('flood:: exceed time limit title'));
+echo lang::translate('flood: Max Amount of posts is');
+echo $max_posts; 
+echo lang::translate('flood: per');
+echo $interval;
+echo lang::translate('flood: seconds'); 
+echo "<br />\n";
+//echo "Max Amount of posts is $max_posts per $interval secs\n";
+echo lang::translate('flood: Your post counter will be reset in');
+echo $res['minutes'];
+echo lang::translate('flood: minutes and'); 
+echo $res['seconds'];
+echo lang::translate('flood: seconds'); 
+
