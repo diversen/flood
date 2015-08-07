7fa41fed7a3f77a554144c2da35b9af74ab2aea0
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 19:12:50 +0200

    language

---
+++ lang/en/language.php
@@ -0,0 +1,16 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/flood/module.php
+
+$LANG['flood: You can post agian'] = 'flood: You can post agian';
+$LANG['flood: You should be able to post'] = 'flood: You should be able to post';
+$LANG['flood:: exceed time limit title'] = 'flood:: exceed time limit title';
+$LANG['flood: Max Amount of posts is'] = 'flood: Max Amount of posts is';
+$LANG['flood: per'] = 'flood: per';
+$LANG['flood: days and'] = 'flood: days and';
+$LANG['flood: hours and'] = 'flood: hours and';
+$LANG['flood: minutes and'] = 'flood: minutes and';
+$LANG['flood: seconds'] = 'flood: seconds';
+$LANG['flood: Your post counter will be reset in'] = 'flood: Your post counter will be reset in';
